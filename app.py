635d67e317b081c5bdf6f462e254d86c1ea247df
sahilpinjari1612 <sahilpinjari1612@gmail.com>
2025-08-03 19:50:59 +0530

    BindFlask app to 0.0.0.0 and dynamic port for render

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask, render_template, json
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    with open("data/offers.json", "r") as f:
-        offers = json.load(f)
-    return render_template("index.html", offers=offers)
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
+    return "ReferKing is Live!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT via env
+    app.run(host='0.0.0.0', port=port)
+
